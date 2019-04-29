d189c445addbca5ebb2b0152c3e1aa4ba3ecb509
Swap <sp70215@gmail.com>
2019-04-29 14:05:01 -0700

    Completed main MenuParse functions!

--- app/src/main/java/me/switchswap/uscdining/parser/MenuParser.kt
+++ app/src/main/java/me/switchswap/uscdining/parser/MenuParser.kt
@@ -1,23 +1,33 @@
 package me.switchswap.uscdining.parser
 
 import android.annotation.SuppressLint
+import android.content.Context
 import me.switchswap.uscdining.models.DiningHallType
+import me.switchswap.uscdining.models.MealType
+import me.switchswap.uscdining.models.MenuItem
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 /* Class for grabbing and parsing meal data */
-class MenuParser{
+class MenuParser(private val context: Context){
 
     fun buildMenu(location: DiningHallType, date: Date){
         val menuHTML = fetchMenu(location, date)
-        parseMenu(menuHTML)
+
+        // Parse into menu items
+        val menuItems = parseMenu(menuHTML)
+
+        // Insert items into database for easy manipulation
+        val menuManager = MenuManager(context)
+        menuManager.insertItems(menuItems, location)
     }
 
     @SuppressLint("SimpleDateFormat")
-    fun buildUrl(location: DiningHallType, date: Date):String{
+    fun buildUrl(location: DiningHallType, date: Date): String{
         // Format date
         val dateString: String = SimpleDateFormat("MM/dd/yyyy").format(date)
 
@@ -33,28 +43,32 @@ class MenuParser{
         return Jsoup.connect(url).get()
     }
 
-    private fun parseMenu(menuHTML: Document){
+
+    // Parse menu HTML into MenuItem objects
+    private fun parseMenu(menuHTML: Document): ArrayList<MenuItem>{
+        // Array of all the mealItems for each meal type
+        val mealItems = ArrayList<MenuItem>()
+
         // Isolate meal type columns (Breakfast, Brunch, Lunch, Dinner)
         val mealTypeElements = menuHTML.select("div.col-sm-6.col-md-4")
         mealTypeElements.forEachIndexed { _, mealType ->
+
+            // Parse values from html data into array
             val mealTypeTitleElement = mealType.getElementsByClass("menu-venue-title")
             val mealItemElements = mealType.select("ul.menu-item-list > li")
-            println(mealTypeTitleElement.text())
 
             mealItemElements.forEachIndexed { _, item ->
-                println(item.ownText())
+                val itemName = item.ownText()
+                val allergens = ArrayList<String>()
 
-                // Get item allergens
+                // Get item allergens and add them to list
                 val mealItemAllergenElements = item.select("span.fa-allergen-container > i > span")
-                mealItemAllergenElements.forEachIndexed{ _, allergen ->
-                    print(allergen.text() + " ")
+                mealItemAllergenElements.forEachIndexed { _, allergen ->
+                    allergens.add(allergen.text())
                 }
-                println()
-                println()
+                mealItems.add(MenuItem(itemName, allergens, MealType.valueOf(mealTypeTitleElement.text().toUpperCase())))
             }
         }
+        return mealItems
     }
-
-
 }
-// INSERT INTO table (id, name, age) VALUES(1, "A", 19) ON DUPLICATE KEY UPDATE name="A", age=19
