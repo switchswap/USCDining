431eff4f0c5536892ada1d72bc5608aec4be3c42
Swap <sp70215@gmail.com>
2019-05-30 16:40:58 -0700

    Deleted MenuUtil class
    No longer needed

--- app/src/main/java/me/switchswap/uscdining/util/MenuUtil.kt
+++
@@ -1,8 +0,0 @@
-package me.switchswap.uscdining.util
-
-public class MenuUtil{
-//    "https://hospitality.usc.edu/residential-dining-menus//?menu_venue=venue-27229&menu_date=01/31/2019" //village
-//    "https://hospitality.usc.edu/residential-dining-menus//?menu_venue=venue-518&menu_date=01/31/2019" // Parkside
-//    "https://hospitality.usc.edu/residential-dining-menus//?menu_venue=venue-514&menu_date=01/31/2019" // EVK
-
-}
