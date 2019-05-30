26fff76460fe3a263f0227e1bf39f68fe2e14dff
Swap <sp70215@gmail.com>
2019-05-30 16:36:40 -0700

    Added dining hall type to MenuItem class

--- app/src/main/java/me/switchswap/uscdining/models/MenuItem.kt
+++ app/src/main/java/me/switchswap/uscdining/models/MenuItem.kt
@@ -1,9 +1,16 @@
 package me.switchswap.uscdining.models
 
 /* Custom data type for MenuItems */
-data class MenuItem (val itemName: String, val allergens: ArrayList<String>,
-                     val mealType: MealType) {
-    fun hasAllergen(allergen: String): Boolean{
+data class MenuItem (val itemName: String,
+                     val allergens: ArrayList<String>,
+                     val mealType: MealType,
+                     val diningHallType: DiningHallType) {
+
+    fun hasAllergen(allergen: String): Boolean {
         return allergen in allergens
+    }
+
+    fun getAllergenString(): String {
+        return allergens.joinToString(separator = ", ")
     }
 }
