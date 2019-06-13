ace36192b94774ac0d53ab3d9b96a684dc2cdb7a
Swap <sp70215@gmail.com>
2019-06-13 01:20:01 -0700

    Transition to MaterialDialogs and refine app logic
    Now loads data on startup and writes date to shared prefs

--- app/src/main/java/me/switchswap/uscdining/ui/activities/MainActivity.kt
+++ app/src/main/java/me/switchswap/uscdining/ui/activities/MainActivity.kt
@@ -1,7 +1,7 @@
 package me.switchswap.uscdining.ui.activities
 
-import android.app.Activity
-import android.content.Intent
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import com.google.android.material.navigation.NavigationView
@@ -15,16 +15,21 @@ import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.fragment.app.FragmentTransaction
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import androidx.viewpager.widget.ViewPager
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.datetime.datePicker
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.fragment_menu.*
 import me.switchswap.uscdining.R
 import me.switchswap.uscdining.models.DiningHallType
 import me.switchswap.uscdining.parser.MenuManager
 import me.switchswap.uscdining.parser.database
 import me.switchswap.uscdining.ui.adapters.MenuPagerAdapter
-import me.switchswap.uscdining.ui.fragments.DatePickerFragment
+import me.switchswap.uscdining.util.DateUtil
 import org.jetbrains.anko.db.delete
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.longToast
@@ -32,6 +37,8 @@ import org.jetbrains.anko.toast
 import java.util.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private val dateUtil = DateUtil()
+
     private val viewPager by lazy(LazyThreadSafetyMode.NONE) {
         findViewById<ViewPager>(R.id.viewpager)
     }
@@ -39,16 +46,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private val tabs by lazy(LazyThreadSafetyMode.NONE) {
         findViewById<TabLayout>(R.id.tablayout)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        // Set button listener
-        fab.setOnClickListener {
-            val datePickerFragment = DatePickerFragment()
-            datePickerFragment.show(supportFragmentManager, "datePicker")
-        }
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
@@ -57,20 +61,49 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Set navigation drawer listener
         nav_view.setNavigationItemSelectedListener(this)
 
-
         // Set action bar date
         setActionBarDate()
 
-        // Initialize views and setup view pager
-        setupViewPager(R.id.nav_evk)
+        // Set system prefs date
+        dateUtil.writeDate(this, dateUtil.convertDate(Calendar.getInstance()))
 
-        // Delete database values [DEBUG]
-        database.use{
-            delete("MenuItems")
-            delete("ItemAllergens")
+        // Initialize views and setup view pager
+        setupViewPager()
+
+        // Set refresh layout listener
+        val refreshLayout: SwipeRefreshLayout = findViewById(R.id.refresh_layout)
+        refreshLayout.apply {
+            setColorSchemeResources(R.color.colorAccent)
+
+            setOnRefreshListener {
+                // On refresh, use date from prefs
+                reloadMenu(dateUtil.readDate(this@MainActivity))
+            }
+        }
+
+        // Set button listener
+        fab.setOnClickListener {
+            MaterialDialog(this).show {
+                val currentDate = Calendar.getInstance().apply {
+                    val date = dateUtil.readDate(this@MainActivity)
+                    time = Date(dateUtil.readDate(this@MainActivity))
+                }
+                datePicker(currentDate = currentDate) { _, date ->
+                    // Use date (Calendar)
+                    setActionBarDate(date)
+
+                    // Update shared preference
+                    val unixTimeStamp = dateUtil.convertDate(date)
+                    dateUtil.writeDate(this@MainActivity, unixTimeStamp)
+
+                    // Reload menu
+                    reloadMenu(unixTimeStamp)
+                }
+            }
         }
 
         // Populate database from website if needed
+        reloadMenu(dateUtil.readDate(this))
     }
 
     override fun onBackPressed() {
@@ -93,21 +126,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // as you specify a parent activity in AndroidManifest.xml.
         return when(item.itemId) {
             R.id.action_refresh -> {
-                // Populate database from website
-                doAsync{
-                    val menuManager = MenuManager(this@MainActivity)
-                    val databasePopulated = menuManager.populateDatabaseFromWebsite(this@MainActivity, Date(1548971445000))
-
-                    runOnUiThread{
-                        if(databasePopulated){
-                            reloadFragments(supportFragmentManager.fragments)
-                            toast("Success!")
-                        }
-                        else{
-                            longToast("Something went wrong!")
-                        }
-                    }
-                }
+                //reloadMenu(1548971445000)
                 true
             }
             else -> super.onOptionsItemSelected(item)
@@ -116,16 +135,36 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
-        setupViewPager(item.itemId)
+        changeViewPager(item.itemId)
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+
+    /**
+     * Setup [ViewPager] such that if it is scrolled horizontally, the [SwipeRefreshLayout] is not
+     * activated by accident
+     */
+    private fun setupViewPager(){
+        viewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageSelected(position: Int) {}
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+            // If scroll state is not idle, disable SwipeRefreshLayout
+            override fun onPageScrollStateChanged(state: Int) {
+                enableDisableSwipeRefresh(state == ViewPager.SCROLL_STATE_IDLE)
+            }
+        })
+
+        changeViewPager(R.id.nav_evk)
     }
 
     /**
      * @param selectedItem is the item selected from the navigation drawer
      * @see onOptionsItemSelected
      */
-    private fun setupViewPager(@IdRes selectedItem : Int) {
+    private fun changeViewPager(@IdRes selectedItem : Int) {
         val fragmentPagerAdapter : FragmentStatePagerAdapter? = when(selectedItem) {
             R.id.nav_evk -> {
                 Log.i("setupViewPager", "EVK Selected")
@@ -148,8 +187,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    private fun setActionBarDate() {
-        val calendar = Calendar.getInstance()
+    private fun setActionBarDate(calendar: Calendar = Calendar.getInstance()) {
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
         val day = calendar.get(Calendar.DAY_OF_MONTH)
@@ -169,7 +207,59 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
         }
     }
-    // todo: fix date or use string throughout
-    // todo: background load needed on first menu creation, date change
-    // todo: make gui look pretty / make allergens have custom colored border
+
+    /**
+     * Reloads menu by checking database for items
+     * If no items are found for a given day, populate database from website and load from there
+     *
+     * @param unixTimeStamp is the date for which to retrieve the menu
+     */
+    private fun reloadMenu(unixTimeStamp: Long?) {
+        if(unixTimeStamp == null) return
+
+        val refreshLayout: SwipeRefreshLayout = findViewById(R.id.refresh_layout)
+        refreshLayout.isRefreshing = true
+
+
+        doAsync{
+            val menuManager = MenuManager(this@MainActivity)
+
+            // Check if data exists already
+            var menuExists: Boolean = menuManager.checkMenuExists(null, dateUtil.readDate(this@MainActivity))
+
+            if(!menuExists){
+                // Populate database from website
+                Log.d("action_refresh", "Attempting to populate database...")
+                menuExists = menuManager.populateDatabaseFromWebsite(Date(unixTimeStamp))
+            }
+
+
+            runOnUiThread{
+                if(menuExists){
+                    Log.d("action_refresh", "Database populated.")
+                    reloadFragments(supportFragmentManager.fragments)
+                    toast("Success!")
+                }
+                else{
+                    Log.d("action_refresh", "Something went wrong.")
+                    longToast("Something went wrong!")
+                }
+                refreshLayout.isRefreshing = false
+            }
+        }
+    }
+
+    /**
+     * Enables or disables [SwipeRefreshLayout]
+     */
+    private fun enableDisableSwipeRefresh(enable: Boolean) {
+        val swipeRefreshLayout = findViewById<SwipeRefreshLayout>(R.id.refresh_layout)
+        if(swipeRefreshLayout != null) {
+            swipeRefreshLayout.isEnabled = enable
+        }
+    }
+    // todo: Make empty dining halls greyed out
+    // todo: Make empty "times" in the day greyed out
+    // todo: Handle brunch. Make Lunch grey w/ no items and Breakfast gold with brunch items
+
 }
--- app/src/main/java/me/switchswap/uscdining/util/DateUtil.kt
+++ app/src/main/java/me/switchswap/uscdining/util/DateUtil.kt
@@ -1,5 +1,10 @@
 package me.switchswap.uscdining.util
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.widget.TextView
+import me.switchswap.uscdining.R
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -10,6 +15,8 @@ class DateUtil {
      * @return A time format of MM dd yyyy
      *
      * Converts a date string of format MM/dd/yyyy into a unix time stamp
+     *
+     * Converts a Calendar to a unix time stamp
      * @return A unix time stamp
      */
     fun convertDate(unixTimeStamp: Long): String? {
@@ -24,5 +31,40 @@ class DateUtil {
         val date: Date = simpleDateFormat.parse(dateString)
         return date.time
     }
+    fun convertDate(calendar: Calendar): Long? {
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        return convertDate("${month + 1}/$day/$year")
+    }
 
+    /**
+     * Read date from shared preferences as a unix timestamp
+     * If no activity is given, returns -1
+     *
+     * @param activity is the activity to call this from
+     */
+    fun readDate(activity: Activity?): Long {
+        if(activity == null) return -1
+        val currentDate: Long = convertDate(Calendar.getInstance())!!
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return currentDate
+        return sharedPref.getLong(activity.getString(R.string.pref_menu_date), currentDate)
+    }
+
+    /**
+     * Write date to shared preferences as a unix timestamp
+     * If no date is given, no date is written
+     *
+     * commit() instead of apply() because this should be done synchronously
+     */
+    @SuppressLint("ApplySharedPref")
+    fun writeDate(activity: Activity?, unixTimeStamp: Long?) {
+        if(activity == null || unixTimeStamp == null) return
+
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            putLong(activity.getString(R.string.pref_menu_date), unixTimeStamp)
+            commit()
+        }
+    }
 }
