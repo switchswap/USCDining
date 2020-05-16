82cb06bc130d7be39dcd600f6e90e3aa005cf964
Swapnil Patel <sp70215@gmail.com>
2020-05-16 02:25:41 -0700

    Add usc-menu-kt depencency
    Added USC-Menu-kt for dining menu fetching.

--- app/src/main/java/me/switchswap/uscdining/models/MealType.kt
+++
@@ -1,8 +0,0 @@
-package me.switchswap.uscdining.models
-
-enum class MealType(val typeName: String){
-    BREAKFAST("breakfast"),
-    BRUNCH("brunch"),
-    LUNCH("lunch"),
-    DINNER("dinner")
-}
--- app/src/main/java/me/switchswap/uscdining/models/Menu.kt
+++
@@ -1,7 +0,0 @@
-package me.switchswap.uscdining.models
-
-import kotlin.collections.ArrayList
-
-data class Menu(val menuItems: ArrayList<MenuItem>) {
-    // Function to return items with specific allergen filters
-}
