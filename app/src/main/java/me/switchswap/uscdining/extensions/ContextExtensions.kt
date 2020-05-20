4f892533a83d8073fbab1bd31447a69f3568fc20
Swapnil Patel <sp70215@gmail.com>
2020-05-20 04:23:50 -0700

    Remove context extension from its class
    The ContextExtensions class was not supposed to exist since that limits the scope of this extension

--- app/src/main/java/me/switchswap/uscdining/extensions/ContextExtensions.kt
+++ app/src/main/java/me/switchswap/uscdining/extensions/ContextExtensions.kt
@@ -3,6 +3,4 @@ package me.switchswap.uscdining.extensions
 import android.content.Context
 import me.switchswap.uscdining.menu.AppDatabase
 
-class ContextExtensions {
-    fun Context?.db() = this?.let { AppDatabase.getInstance(it) }
-}
+fun Context?.db() = this?.let { AppDatabase.getInstance(it) }
