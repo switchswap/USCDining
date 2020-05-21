081dab5f4de7dbfc9419fa136810328e1ee70259
Swapnil Patel <sp70215@gmail.com>
2020-05-21 04:33:41 -0700

    Rewrite DateUtil class to integrate with SharedPreferences
    Completely overhauled this class and added a Shared Preference Listener for other fragments or activities to use upon a date change

--- app/src/main/java/me/switchswap/uscdining/util/DateUtil.kt
+++ app/src/main/java/me/switchswap/uscdining/util/DateUtil.kt
@@ -1,70 +1,87 @@
 package me.switchswap.uscdining.util
 
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
-import android.widget.TextView
+import android.content.SharedPreferences
+import androidx.fragment.app.FragmentActivity
 import me.switchswap.uscdining.R
 import java.text.SimpleDateFormat
 import java.util.*
 
-class DateUtil {
+/**
+ * @param activity is the activity to call this from
+ */
+class DateUtil(private val activity: FragmentActivity?) {
 
-    /**
-     * Converts unix time representation into current readable time
-     * @return A time format of MM dd yyyy
-     *
-     * Converts a date string of format MM/dd/yyyy into a unix time stamp
-     *
-     * Converts a Calendar to a unix time stamp
-     * @return A unix time stamp
-     */
-    fun convertDate(unixTimeStamp: Long): String? {
-        if (unixTimeStamp != 0L) {
-            val dateFormat = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
-            return dateFormat.format(Date(unixTimeStamp))
-        }
-        return null
+    private val sharedPreferences by lazy {
+        activity?.getPreferences(Context.MODE_PRIVATE)
     }
-    fun convertDate(dateString: String): Long? {
-        val simpleDateFormat = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
-        val date: Date = simpleDateFormat.parse(dateString)
-        return date.time
-    }
-    fun convertDate(calendar: Calendar): Long? {
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-        return convertDate("${month + 1}/$day/$year")
-    }
+
+    fun subscribe(onSharedPreferenceChangeListener: SharedPreferences.OnSharedPreferenceChangeListener) =
+            sharedPreferences?.registerOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener)
+
+    fun unSubscribe(onSharedPreferenceChangeListener: SharedPreferences.OnSharedPreferenceChangeListener) =
+            sharedPreferences?.registerOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener)
 
     /**
      * Read date from shared preferences as a unix timestamp
      * If no activity is given, returns -1
      *
-     * @param activity is the activity to call this from
      */
-    fun readDate(activity: Activity?): Long {
+    fun readDate(): Long {
         if(activity == null) return -1
         val currentDate: Long = convertDate(Calendar.getInstance())!!
-        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return currentDate
-        return sharedPref.getLong(activity.getString(R.string.pref_menu_date), currentDate)
+        return sharedPreferences?.getLong(activity.getString(R.string.pref_menu_date), currentDate) ?: currentDate
     }
 
     /**
      * Write date to shared preferences as a unix timestamp
      * If no date is given, no date is written
      *
-     * commit() instead of apply() because this should be done synchronously
+     * We're using apply because we will depend on subscription to be picked up by our
+     * [me.switchswap.uscdining.ui.fragments.MenuFragment]
      */
-    @SuppressLint("ApplySharedPref")
-    fun writeDate(activity: Activity?, unixTimeStamp: Long?) {
+    fun writeDate(unixTimeStamp: Long?) {
         if(activity == null || unixTimeStamp == null) return
 
-        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return
-        with (sharedPref.edit()) {
+        sharedPreferences?.edit()?.apply {
             putLong(activity.getString(R.string.pref_menu_date), unixTimeStamp)
-            commit()
+            apply()
+        }
+    }
+
+    companion object {
+
+        /**
+         * Converts unix time representation into current readable time
+         * @return A time format of MM dd yyyy
+         *
+         * Converts a date string of format MM/dd/yyyy into a unix time stamp
+         *
+         * Converts a Calendar to a unix time stamp
+         * @return A unix time stamp
+         */
+        fun convertDate(unixTimeStamp: Long): String? {
+            if (unixTimeStamp != 0L) {
+                val dateFormat = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
+                return dateFormat.format(Date(unixTimeStamp))
+            }
+            return null
+        }
+
+        /**
+         * [SimpleDateFormat.parse] can produce null so the variable date can be null
+         */
+        fun convertDate(dateString: String): Long? {
+            val simpleDateFormat = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
+            val date = simpleDateFormat.parse(dateString)
+            return date?.time
+        }
+
+        fun convertDate(calendar: Calendar): Long? {
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+            return convertDate("${month + 1}/$day/$year")
         }
     }
 }
