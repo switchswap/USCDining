bfdfb175a760b3fd8adca5f9a03d10205db003ef
Swapnil Patel <sp70215@gmail.com>
2020-07-17 17:49:44 -0700

    Create FragmentInteractionListener
    This class provides util functions for the MenuFragment to communicate with the MainActivity

--- app/src/main/java/me/switchswap/uscdining/ui/interfaces/FragmentInteractionListener.kt
+++ app/src/main/java/me/switchswap/uscdining/ui/interfaces/FragmentInteractionListener.kt
@@ -5,4 +5,24 @@ interface FragmentInteractionListener {
      * Update Tab color to indicate brunch
      */
     fun makeTabBrunch()
+
+    /**
+     * Updates the navigation view depending on which dining halls are open
+     */
+    fun configureDiningHalls()
+
+    /**
+     * Ge refreshing status
+     */
+    fun getRefreshing(): Boolean
+
+    /**
+     * Set refreshing status
+     */
+    fun setRefreshing(status: Boolean)
+
+    /**
+     * Check if empty dining hall
+     */
+    // fun isEmptyHall(): Boolean
 }
