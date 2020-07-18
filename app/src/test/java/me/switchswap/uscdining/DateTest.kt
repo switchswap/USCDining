c5eeebc9019e9c0a1777ad687ebe311958f9248e
Swapnil Patel <sp70215@gmail.com>
2020-07-17 20:20:26 -0700

    Update DateUtil reference
    Functions are static so the object no longer needs to be built

--- app/src/test/java/me/switchswap/uscdining/DateTest.kt
+++ app/src/test/java/me/switchswap/uscdining/DateTest.kt
@@ -6,17 +6,16 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class DateTest {
-    private val dateUtil = DateUtil()
 
     @Test
     fun epochToString_Test(){
-        val dateString: String? = dateUtil.convertDate(1548921600000)
+        val dateString: String? = DateUtil.convertDate(1548921600000)
         assertEquals(dateString, "01/31/2019")
     }
 
     @Test
     fun stringToEpoch_Test(){
-        val unixTimeStamp: Long? = dateUtil.convertDate("01/31/2019")
+        val unixTimeStamp: Long? = DateUtil.convertDate("01/31/2019")
         assertEquals(unixTimeStamp, 1548921600000)
     }
 }
