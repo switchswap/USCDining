1ae2958656f963b85c028dc05a374b2ec6310f44
Swapnil Patel <sp70215@gmail.com>
2020-07-21 15:24:17 -0700

    Create atomic variable to ensure coroutine is only called once

--- app/src/main/java/me/switchswap/uscdining/data/MenuFragmentViewModel.kt
+++ app/src/main/java/me/switchswap/uscdining/data/MenuFragmentViewModel.kt
@@ -1,5 +1,6 @@
 package me.switchswap.uscdining.data
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,19 +9,23 @@ import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 import me.switchswap.diningmenu.models.DiningHallType
 import me.switchswap.diningmenu.models.ItemType
+import java.util.concurrent.atomic.AtomicBoolean
 
 class MenuFragmentViewModel: ViewModel() {
     private lateinit var menuRepository: MenuRepository
 
+    private val isLoading = AtomicBoolean(false)
     val loadingState = MutableLiveData<Boolean>()
 
     fun getMenuData(diningHallType: DiningHallType, itemType: ItemType, date: Long, forceRefresh: Boolean): LiveData<List<MenuItemAndAllergens>> {
-        if (forceRefresh) {
+        if (forceRefresh && !isLoading.get()) {
             viewModelScope.launch(IO) {
+                isLoading.set(true)
                 loadingState.postValue(true)
                 // Todo: Make caching variable again!
                 menuRepository.getMenuFromWeb(date, true)
                 loadingState.postValue(false)
+                isLoading.set(false)
             }
         }
         return menuRepository.getMenuFromDatabase(diningHallType, itemType, date)
@@ -28,5 +33,9 @@ class MenuFragmentViewModel: ViewModel() {
 
     fun setManager(menuDao: MenuDao) {
         menuRepository = MenuRepository(menuDao)
+    }
+
+    companion object {
+        val TAG = MenuFragmentViewModel::class.java.simpleName
     }
 }
